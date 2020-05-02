cceb950b041fe1f7503a173f59f35073ba40a695
gitriyad <riyad.github@gmail.com>
2020-05-02 17:02:26 +0600

    Updates

---
+++ precache-manifest.f910b0eb0641f09dc1a4e28db1f055f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd63d637e7f7715f19596cf37ad83d0",
+    "url": "/ReactWebsite/index.html"
+  },
+  {
+    "revision": "5d9239444a3b2ef8fb09",
+    "url": "/ReactWebsite/static/css/main.2feb10fd.chunk.css"
+  },
+  {
+    "revision": "a01ead32a0a9d0e7db4e",
+    "url": "/ReactWebsite/static/js/2.45069257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWebsite/static/js/2.45069257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9239444a3b2ef8fb09",
+    "url": "/ReactWebsite/static/js/main.4a3a5f7e.chunk.js"
+  },
+  {
+    "revision": "6c737f4dc52868b5ce03",
+    "url": "/ReactWebsite/static/js/runtime-main.2b226136.js"
+  }
+]);
